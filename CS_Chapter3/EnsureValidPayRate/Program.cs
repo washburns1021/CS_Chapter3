c4bf7d89845e47bfcc14c904abbe022ab1917baf
washburns1021 <washburns1021@gmail.com>
2017-02-09 02:52:25 -0500

    ensure
    :thumbsup:

--- CS_Chapter3/EnsureValidPayRate/Program.cs
+++ CS_Chapter3/EnsureValidPayRate/Program.cs
@@ -9,6 +9,20 @@ namespace EnsureValidPayRate
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter hourly pay:");
+            string line = Console.ReadLine();
+            double pay = Convert.ToDouble(line);
+            if (pay < 5.65 || pay > 49.99)
+            {
+                line = Console.ReadLine();
+                pay = Convert.ToDouble(line);
+
+                if (pay < 5.65 || pay > 49.99)
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+            else Console.WriteLine(pay * 40);
         }
     }
 }

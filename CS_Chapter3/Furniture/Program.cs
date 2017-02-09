8b923ba8b97448fb5e088b0c2b7aa28532cfcbcf
washburns1021 <washburns1021@gmail.com>
2017-02-09 03:19:38 -0500

    furniture not finished

--- CS_Chapter3/Furniture/Program.cs
+++ CS_Chapter3/Furniture/Program.cs
@@ -9,6 +9,20 @@ namespace Furniture
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your desired wood");
+            if (Console.ReadLine().Equals("P"))
+            {
+                Console.WriteLine("Pine is 100.00");
+            }
+            else if (Console.ReadLine().Equals("O"))
+                Console.WriteLine("Oak is 225.00");
+
+            else if (Console.ReadLine().Equals("M"))
+                Console.WriteLine("Mahogany is 310.00");
+
+            else Console.WriteLine("0.00");
         }
-    }
-}
+
+        }
+    }   
+        
